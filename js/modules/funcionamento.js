7d764555c58305c240ae6f4e200acec11e51775b
anafsantiago <fla.santiago09@gmail.com>
2025-07-08 22:10:31 -0300

    funcionamento transformado em classe

--- js/modules/funcionamento.js
+++ js/modules/funcionamento.js
@@ -1,17 +1,44 @@
-export default function initFuncionamento() {
-  const funcionamento = document.querySelector("[data-semana]");
+export default class Funcionamento {
+  constructor(funcionamento, activeClass) {
+    this.funcionamento = document.querySelector(funcionamento);
+    this.activeClass = activeClass;
+  }
 
-  const diasSemana = funcionamento.dataset.semana.split(",").map(Number);
-  const horarios = funcionamento.dataset.horario.split(",").map(Number);
+  getDadosFuncionamento() {
+    this.diasSemana = this.funcionamento.dataset.semana.split(",").map(Number);
+    this.horarios = this.funcionamento.dataset.horario.split(",").map(Number);
+  }
 
-  const date = new Date();
-  const diaAgora = date.getDay();
-  const horaAgora = date.getHours();
-  const diaAberto = diasSemana.indexOf(diaAgora) !== -1; //Verifica se o dia atual está presente no array de dias abertos. Se não estiver, retorna -1. Por isso, retornar -1 significa que o local não está aberto naquele dia.
-  //console.log(diasSemana.includes(diaAgora)); --> Faz o mesmo papel que o comando anterior
-  const horaAberta = horaAgora >= horarios[0] && horaAgora < horarios[1];
+  getDadosAgora() {
+    this.date = new Date();
+    this.diaAgora = this.date.getDay();
+    this.horaAgora = this.date.getUTCHours() - 3;
+  }
 
-  if (diaAberto && horaAberta) {
-    funcionamento.classList.add("aberto");
+  estaAberto() {
+    const diaAberto = this.diasSemana.indexOf(this.diaAgora) !== -1;
+
+    const horaAberta =
+      this.horaAgora >= this.horarios[0] && this.horaAgora < this.horarios[1];
+
+    return diaAberto && horaAberta;
+
+    //const diaAberto = diasSemana.indexOf(diaAgora) !== -1; //Verifica se o dia atual está presente no array de dias abertos. Se não estiver, retorna -1. Por isso, retornar -1 significa que o local não está aberto naquele dia.
+    //console.log(this.diasSemana.includes(this.diaAgora)); --> Faz o mesmo papel que o comando anterior
+  }
+
+  ativaAberto() {
+    if (this.estaAberto()) {
+      this.funcionamento.classList.add(this.activeClass);
+    }
+  }
+
+  init() {
+    if (this.funcionamento) {
+      this.getDadosAgora();
+      this.getDadosFuncionamento();
+      this.ativaAberto();
+    }
+    return this;
   }
 }
